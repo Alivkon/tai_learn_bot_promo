a24ba0f02a24cdca92dccd9f90b5eb2e3e2d5765
Alivkon <akonischev@mail.ru>
2024-02-07 20:38:32 +0100

    added questions 16-25

--- tai_card_promo.py
+++ tai_card_promo.py
@@ -6,10 +6,12 @@ from aiogram.utils import executor
 import logging
 from pathlib import Path
 from tai_card_promo_key import token
+from aiogram.types import ParseMode
+from aiogram.utils.markdown import hspoiler
 
 bot = Bot(token)
 dp = Dispatcher(bot)
-
+numask = 0
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 logging.basicConfig(level=logging.INFO)
@@ -20,22 +22,29 @@ async def start(message: types.Message):
     #print("print3")
     img_path = image_file
 
-    caption = "Привет!\nЯ бот для изучения тайского языка. \n\
-    В платной версии ты найдёшь больше вопросов, звуков и ассоциативных картинок. \n\
-    Переходи на @SiamSiam_bot."
+    caption = (
+    f"Привет!\n"
+    f"Меня зовут Сиам-Сиам. Я бот для изучения тайского языка.\n" 
+    f"В платной версии ты найдёшь больше вопросов, звуков и ассоциативных картинок.\n"  
+    f'Переходи на @SiamSiam_bot.')
      # Отправляем изображение с описанием обратно пользователю
     with open(img_path, "rb") as photo:
         await bot.send_photo(message.chat.id, photo=photo, caption=caption)   
     await handle_next_question(message)
 
-@dp.message_handler(commands=["sendpayinformation___"])
+#@dp.message_handler(commands=["sendpayinformation___"])
+@dp.message_handler(commands=[""])
 async def handle_next_question(message: types.Message):
+    global numask 
     # Получаем слова из базы данных
     cursor.execute("SELECT COUNT(*) FROM words")
     (rows,) = cursor.fetchone()
     print("rows= ", int(rows))
-    numask = random.randint(1, rows - 1)
-   # numask = 10    здесь можно задать номер записи
+   # numask = random.randint(1, rows - 1)
+    numask += 1
+    if numask > rows:
+        numask = 1
+    #numask = 10    здесь можно задать номер записи
     print("numask= ", numask)
     cursor.execute(
         "SELECT num, tai, transkript, translate, butn1, butn2, butn3, butn4 FROM words WHERE num = ?",
@@ -70,9 +79,11 @@ async def handle_next_question(message: types.Message):
             await bot.send_photo(chat_id=message.chat.id, photo=photo)
         with open(Path("sound", str(numask) + ".ogg"), "rb") as audio:
             await bot.send_audio(chat_id=message.chat.id, audio=audio)
+            hidden_text = hspoiler(transkript)
         await message.answer(
-            f"{numask}  Выберите правильный перевод:\n  {tai} \n {transkript}",
+            f"{numask}  Выберите правильный перевод:\n  {tai} \n {hidden_text}",
             reply_markup=keyboard,
+            parse_mode=ParseMode.HTML
         )
     else:
         await message.answer("Извините, в базе нет такой строки, наберите /start ")
@@ -83,10 +94,10 @@ async def compare_words(callback_query: types.CallbackQuery):
     # Разбиваем данные из callback на составляющие
     _, btn, translated = callback_query.data.split("_")
     if btn == translated:
-        await callback_query.answer("Правильный ответ!", True)
+        await callback_query.answer("Правильный ответ!", False)
         await handle_next_question(callback_query.message)
     else:
-        await callback_query.answer("Неправильный ответ", True)
+        await callback_query.answer("Неправильный ответ", False)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
